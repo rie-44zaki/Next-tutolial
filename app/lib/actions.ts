9d3199a264e792d09b78e29edd21062ac6845270
rie yoshizaki <r.yoshizaki@hakoreco.com>
2026-06-09 17:53:31 +0900

    12:error.tsx

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,6 +72,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+ 
+  // Unreachable code block
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
