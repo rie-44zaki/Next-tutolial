32ae4d3e2c8c09b818c03024da8ee7cb66a21713
rie yoshizaki <r.yoshizaki@hakoreco.com>
2026-06-12 15:14:10 +0900

    [update]メタ

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,11 @@ import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Login | Acme Dashboard',
+};
 
 export default function LoginForm() {
     const searchParams = useSearchParams();
